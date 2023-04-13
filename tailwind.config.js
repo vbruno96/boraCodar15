32df7324012a52da3b0ee55a2ac7db82891c5a2a
Bruno Vinícius <vbruno96@gmail.com>
2023-04-13 16:26:16 -0300

    add and configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      gray: {
+        50: '#F8FAFC',
+        100: '#F1F5F9',
+        200: '#E2E8F0',
+        300: '#CBD5E1',
+        400: '#94A3B8',
+        500: '#64748B',
+        600: '#475569',
+        700: '#334155',
+        800: '#1E293B',
+        900: '#0F172A',
+      },
+      green: {
+        100: '#04D361',
+        200: '#1B873F',
+      },
+      purple: {
+        100: '#996DFF',
+        200: '#7C3AED',
+      },
+      white: '#FFFFFF',
+      orange: '#FBA94C',
+    },
+    fontFamily: {
+      sans: ['Titillium Web', 'sans-serif'],
+    },
+    fontSize: {
+      sm: ['0.875rem', '1.25'],
+      base: ['1rem', '1.5rem'],
+      lg: ['1.125rem', '1.625rem'],
+      xl: ['2rem', '3rem'],
+    },
+  },
+  plugins: [],
+}
